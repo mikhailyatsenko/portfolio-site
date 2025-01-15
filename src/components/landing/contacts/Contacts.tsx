0e5e1b160a21abbc69939a1d643f38fa70ff83de
mikhailyatsenko <misha.ggg@gmail.com>
2025-01-15 16:36:45 +0100

    wip

--- src/components/landing/contacts/Contacts.tsx
+++ src/components/landing/contacts/Contacts.tsx
@@ -12,23 +12,20 @@ export const Contacts = () => {
       <div className={cls.stars}></div>
       <div className={cls.twinkling}></div>
       <div className="relative z-[2] mx-auto flex h-full w-full flex-col justify-between">
-        <div id="get-in-touch-content" className="h-full w-full">
-          <div className="flex w-full justify-center gap-6 h-sm:gap-12">
-            <div className="w-1/2 max-w-3xl text-gray-500">
-              <h2 className="text-right text-sm">Let&apos;s stay in touch</h2>
-            </div>
-            <div className="w-1/2 text-sm text-gray-500">
-              <p className="text-left">
-                <LocationTime />
-              </p>
-            </div>
+        <div className="flex w-full justify-center gap-6 h-sm:gap-12">
+          <div className="w-1/2 max-w-3xl text-gray-500">
+            <h2 className="text-right text-sm">Let&apos;s stay in touch</h2>
           </div>
-          <div className="mt-6 flex w-full justify-center gap-10 h-sm:gap-20">
-            <div className="flex w-[calc(50%-0.75rem)] justify-end divide-x divide-dashed text-sm h-sm:w-[calc(50%-2.5rem)]">
-              <div className="1/3 pr-6">
-                <p className="mb-2 text-gray-400">Social</p>
-              </div>
-              <div className="1/3 px-6">
+          <div className="w-1/2 text-sm text-gray-500">
+            <p className="text-left">
+              <LocationTime />
+            </p>
+          </div>
+        </div>
+        <div id="get-in-touch-content" className="h-fit w-full">
+          <div className="mt-6 flex w-full justify-center gap-20 h-sm:gap-40">
+            <div className="flex w-[calc(50%-2.5rem)] shrink-0 flex-col justify-end text-sm h-sm:w-[calc(50%-5rem)] md:flex-row">
+              <div className="1/2 px-6">
                 <p className="mb-2 text-gray-400">Email</p>
                 <a
                   href="mailto: yatsenkomike@gmail.com"
@@ -39,7 +36,7 @@ export const Contacts = () => {
                   yatsenkomike@gmail.com
                 </a>
               </div>
-              <div className="1/3 pl-6">
+              <div className="1/2 pl-6">
                 <p className="mb-2 text-gray-400">Social</p>
                 <a
                   className="block leading-6 text-gray-100 transition-colors hover:text-gray-400"
@@ -59,7 +56,7 @@ export const Contacts = () => {
                 </a>
               </div>
             </div>
-            <div className="w-[calc(50%-1.5rem)] shrink-0 h-sm:w-[calc(50%-2.5rem)]">
+            <div className="w-[calc(50%-2.5rem)] shrink-0 h-sm:w-[calc(50%-5rem)]">
               <p className="text-md max-w-full break-words indent-8 text-gray-400">
                 I&apos;m based in Berlin but open to remote work opportunities
                 worldwide. If you&apos;re working on something exciting or want
