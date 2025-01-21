4e7df09008eab2eb642b098a457b0352dcb8e269
mikhailyatsenko <misha.ggg@gmail.com>
2025-01-21 16:35:10 +0100

    lint

--- src/app/[locale]/components/landing/contacts/Footer.tsx
+++ src/app/[locale]/components/landing/contacts/Footer.tsx
@@ -1,16 +1,17 @@
-import { LandingTranslationKeys } from "@/types/i18nTypes";
+import { LandingTranslationKeys } from '@/types/i18nTypes';
 
 export interface FooterProps {
-   t: (key: LandingTranslationKeys) => string;
- 
+  t: (key: LandingTranslationKeys) => string;
 }
 
-export const Footer = ({t}:FooterProps) => {
+export const Footer = ({ t }: FooterProps) => {
   return (
     <footer className="flex w-full justify-between pt-4 text-sm text-gray-500">
-      <p>{t("footer.created_by")} {new Date().getFullYear()}</p>
+      <p>
+        {t('footer.created_by')} {new Date().getFullYear()}
+      </p>
       <a className="text-sm text-gray-100 transition-colors hover:text-gray-400">
-      {t("footer.legal_notice")}
+        {t('footer.legal_notice')}
       </a>
     </footer>
   );
